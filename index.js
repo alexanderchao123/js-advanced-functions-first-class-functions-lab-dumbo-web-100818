bb4415aba2d37de85157943cc7e883168f798ed0
github username <chao.alexander.123@gmail.com>
2018-11-19 15:16:37 -0500

    declared functions with const

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(num) {
+  return function(fare) { return num * fare };
+};
+
+const fareDoubler = function(fare) {
+  return fare * 2;
+};
+
+const fareTripler = function(fare) {
+  return fare * 3;
+};
+
+function fetchSpecifiedDrivers(drivers, callback_func) {
+  return callback_func(drivers);
+}
